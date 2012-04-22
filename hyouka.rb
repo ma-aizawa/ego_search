6f34117f188cebc7c611fa8b3593fec8f9439e16
Masahiro Aizawa <kingyodojo@gmail.com>
2012-04-22 18:11:45 +0900

    きれいにした

--- hyouka.rb
+++ hyouka.rb
@@ -1,11 +1,26 @@
 #! /usr/bin/ruby
+# encoding: utf-8
 
 require 'sinatra'
 require 'uri'
+require 'sass'
 require './search_request'
 
 get '/' do
   haml :index
+end
+
+post '/' do
+  haml :index
+end
+
+before do
+  if request.path_info == '/search' && 
+    (params["search_word"].nil? || 
+    params["search_word"].strip.length == 0)
+    @msg = "検索ワードを入力してください。"
+    request.path_info = '/'
+  end
 end
 
 post '/search' do
@@ -17,4 +32,9 @@ post '/search' do
   @query_string = params["search_word"]
 
   haml :list
+end
+
+get '/*.css' do |path|
+  content_type 'text/css'
+  sass :layout
 end
